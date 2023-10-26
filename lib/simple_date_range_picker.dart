4710c15ee0c0f13564c7f981314d4fc33aecf590
Andy Horn <andyjhorn@gmail.com>
2023-10-25 19:19:18 -0700

    refactor: Remove extra indexing file

--- lib/simple_date_range_picker.dart
+++ lib/simple_date_range_picker.dart
@@ -1,3 +1,4 @@
 library simple_date_range_picker;
 
-export 'package:simple_date_range_picker/src/simple_date_range_picker.dart';
+export 'package:simple_date_range_picker/src/widgets/simple_date_range_picker.dart';
+export 'package:simple_date_range_picker/src/show_simple_date_range_picker_dialog.dart';
--- lib/src/simple_date_range_picker.dart
+++
@@ -1,2 +0,0 @@
-export 'package:simple_date_range_picker/src/widgets/simple_date_range_picker.dart';
-export 'package:simple_date_range_picker/src/show_simple_date_range_picker_dialog.dart';
