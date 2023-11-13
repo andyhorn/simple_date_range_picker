da3a737a552e6aa322a3cb1a79cfec421d9a2141
Andy Horn <andyjhorn@gmail.com>
2023-11-13 09:40:22 -0700

    feat: Move selection handler into config
    Move the DateTime selection logic into the `SimpleDateRangePickerRange` class.
    
    This helps separate the concerns of the picker and the config into presentation and business logic.

--- lib/src/models/simple_date_range_picker_config.dart
+++ lib/src/models/simple_date_range_picker_config.dart
@@ -4,41 +4,83 @@ sealed class SimpleDateRangePickerConfig {
   const SimpleDateRangePickerConfig();
 
   List<DateTime> get dates;
+  void onSelected(DateTime date);
 }
 
 class SimpleDateRangePickerRange extends SimpleDateRangePickerConfig {
-  const SimpleDateRangePickerRange({
+  SimpleDateRangePickerRange({
     required this.initialDateRange,
     required this.onChanged,
-  });
+  })  : _startDate = initialDateRange?.start,
+        _endDate = initialDateRange?.end;
 
   final DateTimeRange? initialDateRange;
   final ValueChanged<DateTimeRange?> onChanged;
+  DateTime? _startDate;
+  DateTime? _endDate;
+
+  @override
+  void onSelected(DateTime date) {
+    // selecting the start date again resets the selection
+    if (date == _startDate) {
+      _startDate = null;
+      // selecting the end date again resets the selection
+    } else if (date == _endDate) {
+      _endDate = null;
+      // selecting a new date when both ends of the range have been selected
+      // resets the selection with the new date as the start date
+    } else if (_startDate != null && _endDate != null) {
+      _endDate = null;
+      _startDate = date;
+      // selecting a date that lies after the end date moves the start date
+      // to the current end date and sets the end date to the newly selected
+      // date
+    } else if (_endDate != null && date.isAfter(_endDate!)) {
+      _startDate = _endDate;
+      _endDate = date;
+      // selecting a date that lies before the current start date sets it as
+      // the new start date
+    } else if (_startDate != null && date.isBefore(_startDate!)) {
+      _startDate = date;
+      // if no end date has been selected, the new date becomes the end date
+    } else if (_startDate != null && _endDate == null) {
+      _endDate = date;
+      // if all other conditions are false, this newly selected date is the
+      // beginning of a new date range
+    } else {
+      _startDate = date;
+    }
+
+    if (_startDate == null || _endDate == null) {
+      onChanged(null);
+    } else {
+      onChanged(DateTimeRange(start: _startDate!, end: _endDate!));
+    }
+  }
 
   @override
   List<DateTime> get dates {
-    if (initialDateRange == null) {
+    if (_startDate == null && _endDate == null) {
       return const [];
     }
 
-    var dates = <DateTime>[];
-    var date = initialDateRange!.start;
+    if (_endDate == null) {
+      return [_startDate!];
+    }
 
-    while (date.isBefore(initialDateRange!.end)) {
+    if (_startDate == null) {
+      return [_endDate!];
+    }
+
+    var dates = <DateTime>[];
+    var date = _startDate!;
+
+    while (date.isBefore(_endDate!)) {
       dates.add(date);
       date = date.add(const Duration(days: 1));
     }
 
-    dates.add(initialDateRange!.end);
+    dates.add(_endDate!);
     return dates;
-  }
-
-  SimpleDateRangePickerRange copyWith({
-    DateTimeRange? dateRange,
-  }) {
-    return SimpleDateRangePickerRange(
-      initialDateRange: dateRange,
-      onChanged: onChanged,
-    );
   }
 }
--- lib/src/widgets/simple_date_range_picker.dart
+++ lib/src/widgets/simple_date_range_picker.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:simple_date_range_picker/simple_date_range_picker.dart';
 import 'package:simple_date_range_picker/src/constants/constants.dart';
-import 'package:simple_date_range_picker/src/extensions/date_time_extensions.dart';
 import 'package:simple_date_range_picker/src/widgets/month_grid.dart';
 import 'package:simple_date_range_picker/src/widgets/month_title.dart';
 
@@ -22,53 +21,22 @@ class SimpleDateRangePicker extends StatefulWidget {
 }
 
 class _SimpleDateRangePickerState extends State<SimpleDateRangePicker> {
-  late SimpleDateRangePickerConfig _selection = widget.config;
+  late final config = widget.config;
   late DateTime currentMonth;
-  DateTime? startDate;
-  DateTime? endDate;
-
-  List<DateTime> get selection {
-    return _selection.dates;
-  }
-
-  set selection(List<DateTime> value) {
-    switch (widget.config) {
-      case SimpleDateRangePickerRange(:final onChanged):
-        final dateRange = value.isEmpty
-            ? null
-            : DateTimeRange(
-                start: value.first,
-                end: value.last,
-              );
-
-        _selection = SimpleDateRangePickerRange(
-          initialDateRange: dateRange,
-          onChanged: onChanged,
-        );
-
-        onChanged(dateRange);
-    }
-  }
 
   @override
   void initState() {
     super.initState();
 
-    return switch (widget.config) {
+    return switch (config) {
       SimpleDateRangePickerRange(initialDateRange: final dateRange) => () {
           if (dateRange != null) {
-            // _selection = dateRange;
-
             currentMonth = DateTime(
               dateRange.start.year,
               dateRange.start.month,
               1,
             );
-
-            startDate = dateRange.start;
-            endDate = dateRange.end;
           } else {
-            // selection = const [];
             currentMonth = DateTime.now();
           }
         }(),
@@ -107,9 +75,7 @@ class _SimpleDateRangePickerState extends State<SimpleDateRangePicker> {
           maxWidth: widget.width,
           month: currentMonth,
           selectedDates: {
-            ...selection,
-            if (startDate != null) startDate!,
-            if (endDate != null) endDate!,
+            ...config.dates,
           }.toList(),
           onSelected: _onSelected,
           style: widget.style,
@@ -139,63 +105,7 @@ class _SimpleDateRangePickerState extends State<SimpleDateRangePicker> {
   }
 
   void _onSelected(DateTime date) {
-    switch (widget.config) {
-      case SimpleDateRangePickerRange():
-        // selecting the start date again resets the selection
-        if (date == startDate) {
-          startDate = null;
-          // selecting the end date again resets the selection
-        } else if (date == endDate) {
-          endDate = null;
-          // selecting a new date when both ends of the range have been selected
-          // resets the selection with the new date as the start date
-        } else if (startDate != null && endDate != null) {
-          endDate = null;
-          startDate = date;
-          // selecting a date that lies after the end date moves the start date
-          // to the current end date and sets the end date to the newly selected
-          // date
-        } else if (endDate != null && date.isAfter(endDate!)) {
-          startDate = endDate;
-          endDate = date;
-          // selecting a date that lies before the current start date sets it as
-          // the new start date
-        } else if (startDate != null && date.isBefore(startDate!)) {
-          startDate = date;
-          // if no end date has been selected, the new date becomes the end date
-        } else if (startDate != null && endDate == null) {
-          endDate = date;
-          // if all other conditions are false, this newly selected date is the
-          // beginning of a new date range
-        } else {
-          startDate = date;
-        }
-
-        // after the selection has been made, if there is a selected start and
-        // end date, create the full range of dates
-        if (startDate != null && endDate != null) {
-          selection = _getDateRange(start: startDate!, end: endDate!);
-          // if there is only a start date, empty the range of dates
-        } else if (selection.isNotEmpty) {
-          selection = const [];
-        }
-    }
-
+    config.onSelected(date);
     setState(() {});
-  }
-
-  List<DateTime> _getDateRange({
-    required DateTime start,
-    required DateTime end,
-  }) {
-    final dates = <DateTime>[];
-    var date = start;
-
-    while (date.isBefore(end) || date.isSameDate(end)) {
-      dates.add(date);
-      date = date.add(const Duration(days: 1));
-    }
-
-    return dates;
   }
 }
